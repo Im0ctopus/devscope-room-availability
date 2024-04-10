04559c60cf3442171a36151e0d5f3ebb8926d422
Leo <benemax2001@gmail.com>
2024-04-10 19:11:12 +0100

    Fetching Rooms with my Cookie

---
+++ app/page.tsx
@@ -0,0 +1,36 @@
+type TAppointments = {
+  Subject: string
+  Organizer: string
+  Start: number
+  End: number
+  Private: boolean
+}
+
+type TRoom = {
+  Roomlist: string
+  Name: string
+  RoomAlias: string
+  Email: string
+  Appointments: TAppointments[]
+  Busy: boolean
+}
+
+const Page = async () => {
+  const fetchRooms = async () => {
+    const res = await fetch(process.env.API_LINK ?? '', {
+      headers: {
+        'Content-Type': 'application/json',
+        Cookie: process.env.COOKIE ?? '',
+      },
+      next: { revalidate: 60 },
+    })
+
+    return await res.json()
+  }
+
+  const rooms: TRoom[] = await fetchRooms()
+  console.log(rooms)
+
+  return <div></div>
+}
+export default Page
