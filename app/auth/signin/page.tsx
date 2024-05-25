72a422c0629c015d905a2c819a87de316cdfe5b1
Leo <benemax2001@gmail.com>
2024-05-25 10:51:44 +0100

    mobile ui update

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -6,16 +6,11 @@ export default async function Login() {
   const session = await getServerSession()
   if (session?.user) return redirect('/')
   return (
-    <div className="flex flex-col justify-center items-start gap-2 pt-10 mx-auto w-fit">
-      <img
-        className="w-32"
-        src={
-          (process.env.NODE_ENV === 'production' ? process.env.CDN : '') +
-          `/logos/biglogo.png`
-        }
-        alt="Devscope logo"
-      />
-      <div className="p-5 w-96 flex flex-col justify-center items-start bg-zinc-950 rounded-lg shadow-lg">
+    <div className="flex flex-col justify-center items-center gap-2 py-10 mx-auto px-5 w-full">
+      <div className="w-full max-w-96 flex justify-start">
+        <img className="w-32" src={(process.env.NODE_ENV === 'production' ? process.env.CDN : '') + `/logos/biglogo.png`} alt="Devscope logo" />
+      </div>
+      <div className="p-5 w-full max-w-96 flex flex-col justify-center items-start bg-zinc-950 rounded-lg shadow-lg">
         <h1 className="text-xl font-semibold">Sign In</h1>
         <h2 className="text-sm">Sign in with your Devscope account</h2>
         <div className="w-full flex justify-center items-center mt-10">
--- app/card.tsx
+++ app/card.tsx
@@ -1,4 +1,4 @@
-const CDN = 'https://cdn.jsdelivr.net/gh/Im0ctopus/cdn-devscope-room@1.12' //Change on client side code change
+const CDN = 'https://cdn.jsdelivr.net/gh/Im0ctopus/cdn-devscope-room@1.13' //Change on client side code change
 
 type TWaiting = {
   id: number
@@ -17,42 +17,23 @@ type TRoom = {
   waiting: TWaiting[]
 }
 
-const daysOfWeek = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-]
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 const Card = ({ room }: { room: TRoom }) => {
   const endDate = new Date(room.busyend)
   const startDate = new Date(room.busystart)
   return (
     <div className="p-2 cursor-pointer flex flex-col max-w-96 w-full mx-auto gap-6 rounded-lg relative overflow-hidden bg-zinc-800 transition-all min-h-36 hover:shadow-md shadow-sm shadow-black/50 hover:scale-105 hover:z-20">
-      <div
-        className={`w-full h-2 absolute left-0 top-0 z-10 ${
-          room.busy ? 'bg-red-500' : 'bg-green-500'
-        }`}
-      ></div>
+      <div className={`w-full h-2 absolute left-0 top-0 z-10 ${room.busy ? 'bg-red-500' : 'bg-green-500'}`}></div>
       <div className="w-full flex justify-between items-center text-xl font-medium gap-5">
         <h3>{room.name}</h3>
-        {room.busy ? (
-          <h3 className="text-red-500">Busy</h3>
-        ) : (
-          <h3 className="text-green-500">Open</h3>
-        )}
+        {room.busy ? <h3 className="text-red-500">Busy</h3> : <h3 className="text-green-500">Open</h3>}
       </div>
       <div className="flex justify-between items-center px-3 w-full">
         <div className="w-20 h-16 overflow-clip flex justify-center items-center rounded-xl relative">
           <img
             className="w-full h-full select-none"
-            src={
-              (process.env.NODE_ENV === 'production' ? CDN : '') +
-              `/imgs/${room.name}.jpg`
-            }
+            src={(process.env.NODE_ENV === 'production' ? CDN : '') + `/imgs/${room.name}.jpg`}
             alt={room.name}
           />
         </div>
@@ -63,8 +44,7 @@ const Card = ({ room }: { room: TRoom }) => {
               <p>{daysOfWeek[startDate.getDay()]}</p>
               <p>{startDate.getDate()},</p>
               <p>
-                {startDate.getHours()}:
-                {('0' + startDate.getMinutes()).slice(-2)}
+                {startDate.getHours()}:{('0' + startDate.getMinutes()).slice(-2)}
               </p>
               <p>-</p>
               <p>
